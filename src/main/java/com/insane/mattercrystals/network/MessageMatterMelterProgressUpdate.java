9ced9eb20bc7775eb27a76c0536f7555a11b2a6c
Michael Lydeamore <mlydeamore@student.unimelb.edu.au>
2015-08-21 09:36:40 +1000

    Add texture files

---
+++ src/main/java/com/insane/mattercrystals/network/MessageMatterMelterProgressUpdate.java
@@ -0,0 +1,62 @@
+package com.insane.mattercrystals.network;
+
+import io.netty.buffer.ByteBuf;
+import net.minecraft.client.Minecraft;
+import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
+
+import com.insane.mattercrystals.tileentity.TileMatterMelter;
+
+import cpw.mods.fml.common.network.ByteBufUtils;
+import cpw.mods.fml.common.network.simpleimpl.IMessage;
+import cpw.mods.fml.common.network.simpleimpl.IMessageHandler;
+import cpw.mods.fml.common.network.simpleimpl.MessageContext;
+
+public class MessageMatterMelterProgressUpdate implements IMessage {
+	
+	public MessageMatterMelterProgressUpdate() {}
+
+	public int x, y, z;
+	public byte progress;
+
+	public MessageMatterMelterProgressUpdate(int x, int y, int z, byte progress)
+	{
+		this.x=x;
+		this.y=y;
+		this.z=z;
+		this.progress = progress;
+	}
+
+	@Override
+	public void toBytes(ByteBuf buf)
+	{
+		buf.writeInt(x);
+		buf.writeInt(y);
+		buf.writeInt(z);
+		buf.writeByte(progress);
+	}
+
+	@Override
+	public void fromBytes(ByteBuf buf)
+	{		
+		this.x = buf.readInt();
+		this.y = buf.readInt();
+		this.z = buf.readInt();
+		
+		this.progress = buf.readByte();
+	}
+	
+	public static final class Handler implements IMessageHandler<MessageMatterMelterProgressUpdate, IMessage>
+	{
+		@Override
+		public IMessage onMessage(MessageMatterMelterProgressUpdate message, MessageContext ctx)
+		{
+			TileEntity te = Minecraft.getMinecraft().theWorld.getTileEntity(message.x, message.y, message.z);
+			if (te != null && te instanceof TileMatterMelter)
+				((TileMatterMelter) te).setProgress(message.progress);
+			
+			return null;
+		}
+	}
+
+}
